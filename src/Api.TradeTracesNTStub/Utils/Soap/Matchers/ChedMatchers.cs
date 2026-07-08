b08adf5f4554b9b5ab7f0cd7918351f08694f0e2
Mohammed Shahin Uddin <mo@thetestroom.com>
2026-07-08 16:09:07 +0100

    CDMS-1435: Permission denined mocked endpoint

--- src/Api.TradeTracesNTStub/Utils/Soap/Matchers/ChedMatchers.cs
+++ src/Api.TradeTracesNTStub/Utils/Soap/Matchers/ChedMatchers.cs
@@ -8,9 +8,17 @@ public static class ChedMatchers
         MessageMatchers.ValidHeaders()
             .Concat(
                 [
-                    new XPathMatcher(MessageMatchers.BodyXPath + "/*[local-name() = 'GetChedCertificateRequest']/*[local-name() = 'ID' and text()]")
+                    new XPathMatcher(MessageMatchers.BodyXPath + "/*[local-name() = 'Envelope']/*[local-name() = 'Body']/*[local-name() = 'GetChedCertificateRequest']/*[local-name() = 'ID' and text() != 'CHEDPP.XI.2026.0000074']")
                 ])
             .ToArray();
+
+    public static IMatcher[] ValidPermissionDenied() =>
+    MessageMatchers.ValidHeaders()
+        .Concat(
+            [
+                new XPathMatcher(MessageMatchers.BodyXPath + "/*[local-name() = 'GetChedCertificateRequest']/*[local-name() = 'ID' and text() = 'CHEDPP.XI.2026.0000074']")
+            ])
+        .ToArray();
     
     public static IMatcher[] ValidCreateAndSubmitChedForDecisionRequest() =>
         MessageMatchers.ValidHeaders()
